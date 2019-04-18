20922e706084cd04cf1b9ae4deb53df9fb897204
evan.admin <evan.muzzall@berkeley.edu>
2019-04-18 10:37:51 -0700

    need to fix Challenge 3 predict on test set

--- solutions/Challenge 3 randomForest solution.R
+++ solutions/Challenge 3 randomForest solution.R
@@ -4,62 +4,52 @@
 
 # 1 Change mtry, # 2 change maxnodes, and fit the model
 set.seed(1)
-rf2 = randomForest(as.factor(train_label) ~ ., 
-                    data = train_X,
-                    ntree = 1000,
+rf2 = ranger::ranger(train_y ~ ., 
+                    data = train_x,
+                    num.trees = 1000,
                     mtry = 3,
-                    maxnodes = 50,
-                    importance = T)
+                    probability = T,
+                    importance = "permutation")
 
 # View results.
 rf2
 
-# Confirm accuracy is 1 - OOB error rate.
-(304 + 114) / nrow(train_X) # 0.7769517
-
-1 - ((304 + 114) / nrow(train_X)) # 0.2230483 - OOB error rate checks out! 
-
 # Variable importance plots; there are different ways to visualize!
-varImpPlot(rf2)
-barchart(rf2$importance, main="rf barchart", col="gray80", border="black") 
-dotplot(rf2$importance, main="rf2 dotplot", col=c(1,6))
+vip::vip(rf2)
+vip::vi(rf2)
 
 # Evaluate performance on the test dataset
-rf2_predicted = predict(rf2, newdata = test_X)
-table(rf2_predicted, test_label)
+rf2_predicted = as.integer(predict(rf2, data = test_x)$predictions[, "1"] > 0.5)
 
 # Calculate test set accuracy
-mean(rf2_predicted == test_label) # 0.7347826
+mean(rf2_predicted == test_y) # 0.7666667
 
 
 ### Challenge 4 iris decision tree solution
 set.seed(1)
-rf_iris = randomForest(as.factor(iris$Species) ~ .,
+rf_iris = ranger::ranger(iris$Species ~ .,
                        data = iris[,-5],
-                       ntree = 1000,
+                       num.trees = 1000,
                        mtry = 3,
-                       maxnodes = 20,
-                       importance = T)
+                       probability = T,
+                       importance = "permutation")
 # View results
 rf_iris
 
-# Compute accuracy
-(50 + 47 + 46) / nrow(iris) # 0.9533333 classification accuracy!
-
-# Double check OOB error
-1 - ((50 + 47 + 46) / nrow(iris)) # OOB = 0.04666667
-
 # plot variable importance
-varImpPlot(rf_iris) 
+vip::vip(rf_iris)
+vip::vi(rf_iris)
 
-# create a quick "test" set comprised of 30 pseudo-randomly selected rows
+# create a quick "test" set comprised of 50 randomly selected rows
 set.seed(1)
 iris_test = iris[sample(which(iris$Species == c("setosa", "versicolor", "virginica")), 50), ]
 iris_test
 
 # Compute performance on new "test" data
-rf_iris_predicted = predict(rf_iris, newdata = iris_test)
-table(rf_iris_predicted, iris_test$Species)
+rf_iris_predicted = predict(rf_iris, data = iris_test)
+str(rf_iris_predicted)
+
+table(rf_iris_predicted$predict, iris_test$Species)
 
 # Calculate test set accuracy 
-mean(rf_iris_predicted == iris_test$Species) # 1  -  perfect prediction! This is quite unlikely to happen in real life and only happened here due to how we designed this example. 
+mean(rf_iris_predicted$predictions == iris_test$Species) # 
